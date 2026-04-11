ca8a718c30593ccd0de264b29ca0464dba6b7859
Marco Todroov <marcosteam7@gmail.com>
2026-04-11 01:33:59 -0400

    removed redundant reconstruction

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -11,11 +11,7 @@ pub fn read() -> Option<ClipboardImage> {
     let mut ctx = arboard::Clipboard::new().ok()?;
 
     if let Ok(img) = ctx.get_image() {
-        let pixels = img
-            .bytes
-            .chunks_exact(4)
-            .flat_map(|p| [p[0], p[1], p[2], p[3]])
-            .collect();
+        let pixels = img.bytes.into_owned();
         return Some(ClipboardImage::Pixels(ImageData::new(
             pixels,
             img.width as u32,
